c389701bfce781c9df942dfae7d7d34cff88d278
mongsam2 <songjaehyun121476@gmail.com>
2024-09-05 10:18:33 +0900

    UPDATE Winner model

--- winners/models.py
+++ winners/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Winner(models.Model):
+    rank = models.PositiveIntegerField()
+    win_date = models.DateField()
+    weekly_like = models.PositiveIntegerField(default=0)
+    gallery = models.ForeignKey('galleries.Gallery', on_delete=models.SET_NULL, null=True, balnk=True)
