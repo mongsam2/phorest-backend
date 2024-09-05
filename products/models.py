303bf798c6c07dd6804198b4247194406cf714da
mongsam2 <songjaehyun121476@gmail.com>
2024-09-05 10:14:47 +0900

    UPDATE Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    release_date = models.DateField(auto_now_add=True)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    image = models.ImageField()
+    gallery = models.ForeignKey('galleries.Gallery', on_delete=models.SET_NULL, null=True, blank=True)
