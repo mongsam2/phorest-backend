97d0564fccf5cb1fdeffbe14e75107138643cee7
mongsam2 <songjaehyun121476@gmail.com>
2024-09-05 13:04:25 +0900

    ADD model admin

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Gallery, Background, Category, ImageType
 
 # Register your models here.
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Background)
+class BackgroundAdmin(admin.ModelAdmin):
+    pass 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ImageType)
+class ImageTypeAdmin(admin.ModelAdmin):
+    pass
--- winners/admin.py
+++ winners/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Winner
 
 # Register your models here.
+@admin.register(Winner)
+class WinnerAdmin(admin.ModelAdmin):
+    pass
